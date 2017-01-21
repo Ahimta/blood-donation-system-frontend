707b16c60e98c9e0ccd6d63c1e67ee16f99a78b9
Abdullah Alansari <ahimta@gmail.com>
2017-01-21 17:42:03 +0300

    Add copyrights to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 
-interface IProps { };
-interface IState { };
+const Copyrights = () => {
+  return (<p>
+    <a href='https://linkedin.com/in/ahimta' rel='noopener' target='_blank'>&copy; 2017 Abdullah Alansari</a>
+  </p>);
+};
 
-export default class Footer extends React.Component<IProps, IState> {
-  render() {
-    return (<footer>
-    </footer>);
-  }
+const FlaticonCredits = () => {
+  return (<p>
+    Some icons made by&nbsp;
+    <a href='http://www.flaticon.com/authors/roundicons' rel='noopener' target='_blank' title='Roundicons'>
+      Roundicons
+    </a>&nbsp;
+    from <a href='http://www.flaticon.com' rel='noopener' target='_blank' title='Flaticon'>www.flaticon.com</a>&nbsp;
+    are licensed by&nbsp;
+    <a href='http://creativecommons.org/licenses/by/3.0/' rel='noopener' target='_blank'
+      title='Creative Commons BY 3.0'>
+      CC 3.0 BY
+    </a>
+  </p>);
+};
+
+export default function Footer() {
+  return (<footer className='text-center'>
+    <FlaticonCredits />
+    <Copyrights />
+  </footer>);
 }
