429c486ceb05474ecd50437d00af7d9c784a04c0
Abdullah Alansari <ahimta@gmail.com>
2017-01-21 16:57:31 +0300

    Use same port for socket connection

--- src/app/db.tsx
+++ src/app/db.tsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 
 import IDonation from './types/IDonation';
 
-const socket = io((window.location.hostname === 'localhost') ? 'http://localhost:4000/' : '/');
+const socket = io();
 let cachedLocation = {};
 
 socket.on('connect', () => {
